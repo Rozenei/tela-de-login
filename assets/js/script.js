b56f19915d559c85f931f681edbfd3cbe73f50a4
Rozenei <99339063+Rozenei@users.noreply.github.com>
2022-04-30 11:26:02 -0300

    html css

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+
+
+document.getElementById('btn-submet').addEventListener('click', e => {
+
+    console.log('O botão foi clicado');
+
+});
+
+document.getElementById('form-login').addEventListener('mouseenter', e => {
+
+    console.log('O mouse está sobre o formulário');
+
+});
+
+document.querySelector('#form-login').addEventListener('mouseleave', e=> {
+
+    console.log('O mouse está fora  do formulário');
+
+});
